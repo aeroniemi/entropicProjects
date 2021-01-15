72f1d15e94a72049c85d286211cb06d27ebaef6a
Alex Beavil <alex@aeroniemi.com>
2021-01-15 18:53:07 +0000

    it all works

--- workload/activities.js
+++ workload/activities.js
@@ -2,6 +2,9 @@ const https = require('https');
 const fs = require('fs');
 const apiUrl = "https://data.vatsim.net/v3/vatsim-data.json";
 
+// -----------------------------------------------------------------------------
+// Download related
+// -----------------------------------------------------------------------------
 function downloadLatestData() {
     return new Promise(function (resolve, reject) {
         https.get(apiUrl, function (res) {
@@ -45,7 +48,158 @@ function getNewest() {
         });
     });
 };
-
-
 exports.downloadLatestData = downloadLatestData
 exports.getNewest = getNewest
+// -----------------------------------------------------------------------------
+// Parse-related
+// -----------------------------------------------------------------------------
+const ID_RESUME_FLIGHT = 2
+const ID_NEW_FLIGHT = 1
+const ID_ALREADY_ASSIGNED = 0
+const verbose = false
+function report(text) {
+    if (verbose) console.log(text)
+}
+function returnUniqueCid(table, cid, startTime, groundspeed) {
+    var newCid = cid
+    while (true) {
+        if (newCid in table) {
+            if (table[newCid].logon_time == startTime) {
+                return [newCid, ID_ALREADY_ASSIGNED]
+            } else if (groundspeed > 50) { // assumed to just be dropped data in the middle, fill out as normal
+                report(`Pilot is mid flight - resuming their last flight (last id: ${newCid})`)
+                return [newCid, ID_RESUME_FLIGHT]
+            } else {
+                if (newCid == cid) {
+                    newCid = cid * 10
+                } else {
+                    newCid += 1
+                }
+
+            }
+        } else {
+
+            var timeSinceStart = (new Date() - new Date(startTime)) / 1000
+            report(`Assigned new id to ${cid}: ${newCid}. Flight started ${timeSinceStart} seconds ago`)
+            return [newCid, ID_NEW_FLIGHT]
+        }
+    }
+}
+function validReadings(oldest, newest) {
+    return new Promise(function (resolve, reject) {
+        var output = []
+        var oldestUnix = Math.trunc(oldest.getTime() / 1000);
+        var newestUnix = Math.trunc(newest.getTime() / 1000);
+        fs.readdir("./temp/", (err, files) => {
+            if (err) throw err;
+            if (files === undefined || files.length == 0) {
+                reject("no valid files")
+            }
+            var images = files.map(function (file) {
+                return parseInt(file.substring(0, file.length - 5));
+            })
+            images.forEach((image) => {
+                if (image > oldestUnix && image < newestUnix) {
+                    output.push(image)
+                }
+            })
+            resolve(output)
+        })
+    })
+}
+
+function assembleUsefulSubTable(image) {
+    return new Promise(function (resolve, reject) {
+        var name = image + ".json"
+        fs.readFile("./temp/" + name, (err, body) => {
+            if (err) reject(err)
+            var results = JSON.parse(body)
+            resolve(results)
+        })
+    })
+}
+
+function managePilot(pilot) {
+    return new Promise(function (resolve, reject) {
+        var currentLog = {
+            timestamp: pilot.last_updated,
+            latitude: pilot.latitude,
+            longitude: pilot.longitude,
+            altitude: pilot.altitude,
+            groundspeed: pilot.groundspeed,
+            heading: pilot.heading,
+            qnh_mb: pilot.qnh_mb
+        }
+        var outputPilot = {
+            cid: pilot.cid,
+            name: pilot.name,
+            callsign: pilot.callsign,
+            logon_time: pilot.logon_time,
+            log: [currentLog]
+        }
+        resolve(outputPilot)
+    })
+}
+function managePilots(data) {
+    return new Promise(function (resolve, reject) {
+        var promises = data.pilots.map(function (ele) {
+            return managePilot(ele)
+        })
+        Promise.all(promises).then((results) => {
+            resolve(results)
+        })
+    })
+}
+
+function mergeIdentities(table) {
+    return new Promise(function (resolve, reject) {
+
+        var promises = table.map(function (ele) {
+            return managePilots(ele)
+        })
+        Promise.all(promises).then((results) => {
+            var final = {}
+            results.forEach((result) => {
+                result.forEach((pilot) => {
+                    if (final[pilot.cid]) {
+                        if (final[pilot.cid].logon_time != pilot.logon_time) {
+                            var [newCid, status] = returnUniqueCid(final, pilot.cid, pilot.logon_time, pilot.log[0].groundspeed)
+                            if (status == ID_ALREADY_ASSIGNED) {
+                                final[newCid].log.push(pilot.log[0])
+                            } else if (status == ID_NEW_FLIGHT) {
+                                pilot.initial_logon_time = pilot.logon_time
+                                final[newCid] = pilot
+                            } else if (status == ID_RESUME_FLIGHT) {
+                                if (pilot.callsign == "THY2323") console.log(pilot.callsign, newCid, pilot.logon_time)
+                                final[newCid].logon_time = pilot.logon_time
+                                final[newCid].log.push(pilot.log[0])
+
+                            }
+                        } else {
+                            final[pilot.cid].log.push(pilot.log[0])
+                        }
+
+                    } else {
+                        pilot.initial_logon_time = pilot.logon_time
+                        final[pilot.cid] = pilot
+                    }
+                })
+            })
+            resolve(final)
+        })
+    })
+}
+function assembleUsefulTable(subset) {
+    return new Promise(function (resolve, reject) {
+        var res = subset.map(function (ele) {
+            return assembleUsefulSubTable(ele)
+        })
+        Promise.all(res).then((results) =>
+            resolve(results)
+        )
+
+    })
+}
+exports.assembleUsefulTable = assembleUsefulTable
+exports.mergeIdentities = mergeIdentities
+exports.validReadings = validReadings
--- workload/parse.js
+++ workload/parse.js
@@ -1,144 +1,17 @@
 // create array of readings in this timeframe
 const fs = require('fs');
-function validReadings(oldest, newest) {
-    return new Promise(function (resolve, reject) {
-        var output = []
-        var oldestUnix = Math.trunc(oldest.getTime() / 1000);
-        var newestUnix = Math.trunc(newest.getTime() / 1000);
-        fs.readdir("./temp/", (err, files) => {
-            if (err) throw err;
-            if (files === undefined || files.length == 0) {
-                reject("no valid files")
-            }
-            var images = files.map(function (file) {
-                return parseInt(file.substring(0, file.length - 5));
-            })
-            images.forEach((image) => {
-                if (image > oldestUnix && image < newestUnix) {
-                    output.push(image)
-                }
-            })
-            resolve(output)
-        })
-    })
-}
-function assembleUsefulTable(subset) {
-    return new Promise(function (resolve, reject) {
-        var res = subset.map(function (ele) {
-            return assembleUsefulSubTable(ele)
-        })
-        Promise.all(res).then((results) =>
-            resolve(results)
+var activities = require("./activities.js")
+
+activities.validReadings(new Date("2020-01-14 19:42:00"), new Date())
+    .then((subset) => activities.assembleUsefulTable(subset))
+    .then((table) => activities.mergeIdentities(table))
+    .then((res) => {
+        var array = Object.entries(res)
+        console.log(`Summary Statistics:
+        Flights managed: ${array.length}
+        THY: `
+            , res[1542929]
         )
-
     })
-}
-function assembleUsefulSubTable(image) {
-    return new Promise(function (resolve, reject) {
-        var name = image + ".json"
-        fs.readFile("./temp/" + name, (err, body) => {
-            if (err) reject(err)
-            var results = JSON.parse(body)
-            resolve(results)
-        })
-    })
-}
-
-// async function findIdentities(old, data) {
-//     var output = {
-//         connected_clients: Math.random(),
-//         unique_users: Math.random()
-//     }
-//     if (old == 0) {
-//         return output
-//     }
-//     console.log("hi", old)
-//     old.push(output)
-//     return old
-// }
-
-// function mergeIdentities(table) {
-//     return new Promise(function (resolve, reject) {
-//         var result = []
-//         table.forEach((data) => {
-//             var pilotPromises = data.pilots.map(function (pilot) {
-//                 return managePilotOne(pilot)
-//             })
-//             Promise.all(pilotPromises).then((results) =>
-//                 result.push(results)
-//             )
-
-//         })
-//         resolve(result)
-//     })
-// }
-function managePilotOne(pastTable, pilot) {
-    // return new Promise(function (resolve, reject) {
-    // console.log(pastTable)
-    var pastIndex = -1
-    var currentLog = {
-        timestamp: pilot.last_updated,
-        latitude: pilot.latitude,
-        longitude: pilot.longitude,
-        altitude: pilot.altitude,
-        groundspeed: pilot.groundspeed,
-        heading: pilot.heading,
-        qnh_mb: pilot.qnh_mb
-    }
-    var outputPilot = {
-        cid: pilot.cid,
-        name: pilot.name,
-        callsign: pilot.callsign,
-        logon_time: pilot.logon_time,
-        log: [currentLog]
-    }
-
-    pastTable.forEach((v, i) => {
-        if (v.cid == pilot.cid) {
-            console.log("found a match")
-            outputPilot.log = v.log
-            outputPilot.log.push(currentLog)
-            return [outputPilot, true]
-        }
-    })
-    return [outputPilot, false]
-}
-function findIdentities(last, data) {
-    return new Promise(function (resolve, reject) {
-        // console.log({}, data.pilots[1])
-        var pilot = managePilotOne(last, data.pilots[1])
-        var output = {
-            connected_clients: data.general.connected_clients,
-            unique_users: data.general.unique_users
-        }
-        var cur = last
-        if (pilot[2] == true) {
-
-        } else {
-            cur.push(pilot[1])
-        }
-
-
-        resolve(cur)
-    })
-}
-function mergeIdentities(table) {
-    return new Promise(function (resolve, reject) {
-        // var promises = table.map(function (ele) {
-        //     return findIdentities(ele)
-        // })
-        var base = []
-        table.forEach((v, i) => {
-            // console.log(v.general.unique_users)
-            findIdentities(base, v).then(result => {
-                resolve(result)
-            })
-        })
-        resolve(base)
-    })
-}
-
-validReadings(new Date("2020-01-14 19:42:00"), new Date())
-    .then((subset) => assembleUsefulTable(subset))
-    .then((table) => mergeIdentities(table))
-    .then((res) => console.log(res))
+    // .then((res) => console.log(res))
+    // .then((res) => console.log(res[1208728]))
